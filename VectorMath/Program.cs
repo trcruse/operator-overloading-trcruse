455c0138726073cb464db617faad6df787406670
trcruse <trcruse@F1092-26-2114>
2019-12-09 16:12:13 -0600

    Operator Overloading

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,31 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+
+            Console.WriteLine($"Static method Add: {Vector3.Add(one, two)}");
+
+            Console.WriteLine($"Object method Add: {one.Add(two)}");
+
+            Console.WriteLine($"Operator: {one + two}");
+
+            Console.WriteLine($"Subtraction: {one - two}");
+
+            Console.WriteLine($"Scalar multiplication: {2 * one}");
+
+            Console.WriteLine($"Scalar multiplication: {two * 5}");
+
+            Console.WriteLine($"Should not be equal: {one == two}");
+
+            Console.WriteLine($"Should be equal: {one == new Vector3(3, 4, 5)}");
+            Console.WriteLine($"Preincrement: {++one}");
+            Console.WriteLine($"Postincrement {one++}");
+
+            Console.WriteLine($"Magnitude: {~one}");
+
+
         }
     }
 }
--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -10,6 +10,11 @@ namespace VectorMath
         double Y;
         double Z;
 
+        public override string ToString()
+        {
+            return $"<{this.X}, {this.Y}, {this.Z}>";
+        }
+
         public Vector3(double x, double y, double z)
         {
             X = x;
@@ -18,11 +23,67 @@ namespace VectorMath
         }
 
         // Vector Addition
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
 
-        // Vector Subtraction 
+        public Vector3 Add(Vector3 other)
+        {
+            return new Vector3(this.X + other.X, this.Y + other.Y, this.Z + other.Z);
+        }
+
+        /// <summary>
+        /// No return type
+        /// </summary>
+        /// <param name="other"></param>
+        public void AddInPlace(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        // Vector Subtraction
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
 
         // Vector Scaling 
+        public static Vector3 operator *(double s, Vector3 v)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
+        public static Vector3 operator *(Vector3 v, double s)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
 
         // Vector Comparison
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return a.X != b.X && a.Y != b.Y && a.Z != b.Z;
+        }
+
+        public static Vector3 operator ++(Vector3 a)
+        {
+            return new Vector3(a.X++, a.Y++, a.Z++);
+        }
+
+        public static double operator ~(Vector3 a)
+        {
+            return Math.Sqrt(a.X * a.X + a.Y * a.Y + a.Z + a.Z);
+        }
+
     }
 }
