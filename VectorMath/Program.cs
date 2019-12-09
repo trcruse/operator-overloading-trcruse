a389807944c39f54de89442ed0c30dd54b61e694
Nathan H. Bean <nhbean@ksu.edu>
2019-12-09 15:34:24 -0600

    Initial commit

---
+++ VectorMath/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VectorMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with vectors");
+        }
+    }
+}
---
+++ VectorMath/Vector3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorMath
+{
+    public class Vector3
+    {
+        double X;
+        double Y;
+        double Z;
+
+        public Vector3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // Vector Addition
+
+        // Vector Subtraction 
+
+        // Vector Scaling 
+
+        // Vector Comparison
+    }
+}
